bebfeb9eaba593186d90f65c3b3cb4df07787752
MLTurner <mlturne3@ncsu.edu>
2022-04-04 19:57:14 -0400

    added async await function and connected to local server

--- public/index.js
+++ public/index.js
@@ -1,9 +1,13 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    async function getCounter(){
+    let response = await fetch('http://localhost:9001/counter');
+    let countValue = await response.json();
+    return countValue.value
+    }
 
     function increment(){
         countValue++;
@@ -14,9 +18,11 @@ function main(){
         countValue--;
         countContainer.textContent = countValue;
     }
-
+    
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
-    countContainer.textContent = countValue;
+    countContainer.textContent = await getCounter();
 }
+
 main()
+    
